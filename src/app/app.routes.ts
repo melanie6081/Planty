ae802f24638900d653d59b588b509f79eae001e0
melanie6081 <melanie.feron@etud.univ-jfc.fr>
2024-04-02 18:55:16 +0200

    initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { ConnexionComponent } from './connexion/connexion.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path : 'connexion', component:ConnexionComponent},
+    {path : '', redirectTo: '/connexion', pathMatch: 'full'}
+];
+
+
+
